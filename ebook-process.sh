c78b4f7af485ec913aa6e6183d617a9963b1468b
AndyHazz <andy.nmc@gmail.com>
2016-03-06 23:23:47 +0000

    Update ebook-process.sh
    fixing if statements

--- ebook-process.sh
+++ ebook-process.sh
@@ -14,13 +14,16 @@ KINDLEEMAIL="andyhazz@kindle.com"
 for file in "$@"
 do
   if [ -f $file ]
+  then
     if [[ $file == *.mobi ]]
+    then
       # send to kindle
       mv "$file" "processing/$file"
       mpack -s "${file%.mobi}" "processing/$file" $KINDLEEMAIL
       mv "processing/$file" "sent/$file"
     fi
     if [[ $file == *.epub ]]
+    then
       # convert then send to kindle
       mv "$file" "processing/$file"
       ebook-convert "processing/$file" "converted/${file%.epub}.mobi"

534aacba43e58fe6c8b05928a045bd00c4e957b8
AndyHazz <andy.nmc@gmail.com>
2016-03-06 23:16:36 +0000

    Update ebook-process.sh

--- ebook-process.sh
+++ ebook-process.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 # Script to convert .epub to .mobi files with calibre and email to kindle
 # .mobi files sent straight to kindle
-###########################################
-#Begin config##############################
-#
+######################################################################################
+# Config #######################################################################
+######################################################################################
+
+# Email address for kindle
+KINDLEEMAIL="andyhazz@kindle.com"
+
+######################################################################################
+
+for file in "$@"
+do
+  if [ -f $file ]
+    if [[ $file == *.mobi ]]
+      # send to kindle
+      mv "$file" "processing/$file"
+      mpack -s "${file%.mobi}" "processing/$file" $KINDLEEMAIL
+      mv "processing/$file" "sent/$file"
+    fi
+    if [[ $file == *.epub ]]
+      # convert then send to kindle
+      mv "$file" "processing/$file"
+      ebook-convert "processing/$file" "converted/${file%.epub}.mobi"
+      mpack -s "${file%.epub}" "converted/${file%.epub}.mobi" $KINDLEEMAIL
+      mv "processing/$file" "sent/$file"
+    fi
+  fi
+done
